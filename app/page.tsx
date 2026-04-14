4786322fc976d3a5b392c4b51bc83300fe1e43d0
heae90 <geekexplosion@yahoo.com>
2026-04-14 10:30:32 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -37,16 +37,20 @@ export default function HomePage() {
   <main className="page">
 
     <div className="card" style={{ marginBottom: '20px' }}>
-      <div className="hero-actions">
-        <a className="button button-outline" href="/admin">Admin</a>
-        <a className="button" href="/">Home</a>
-        <a className="button button-outline" href="/#leaderboard">Leaderboard</a>
-        <a className="button button-outline" href="/rules">Rules</a>
-        <a className="button button-outline" href="YOUR_GOOGLE_FORM_LINK" target="_blank">
-          Join
-        </a>
-      </div>
-    </div>
+  <div className="hero-actions">
+    <a className="button" href="/">Home</a>
+    <a className="button button-outline" href="/#leaderboard">Leaderboard</a>
+    <a className="button button-outline" href="/rules">Rules</a>
+    <a
+      className="button button-outline"
+      href="https://docs.google.com/forms/d/e/1FAIpQLSctspZTRdQvHVtaRrOi2I..."
+      target="_blank"
+      rel="noreferrer"
+    >
+      Join
+    </a>
+  </div>
+</div>
       <section className="hero">
         <div>
           <div className="badges">
